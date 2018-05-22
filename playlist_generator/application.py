3cf50f35a44034f305a20d9688d6072272233f01
Nicholas S <blanketastronomer@users.noreply.github.com>
2018-05-21 23:52:54 -0700

    Create a new window

--- playlist_generator/application.py
+++ playlist_generator/application.py
@@ -1,3 +1,14 @@
+from tkinter import *
+
 class Application():
+    def __init__(self):
+        self.main_window = Tk()
+
+    def build_menu(self):
+        pass
+
+    def build_ui(self):
+        pass
+
     def run(self):
-        print("Application is running!")
+        self.main_window.mainloop()
