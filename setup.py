20b5fcdb06bb51da322e7457acf7f1cdc85e8a86
Nicholas S <blanketastronomer@users.noreply.github.com>
2018-05-21 22:52:42 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='playlist_generator',
+    version='0.1.dev0',
+    packages=['playlist_generator'],
+    url='https://github.com/blanketastronomer/playlist_generator',
+    license='MIT',
+    author='blanketastronomer',
+    author_email='blanketastronomer@users.noreply.github.com',
+    description=' A GUI playlist generator for my commandline music player.',
+    python_requires=">=3.6.5"
+)
