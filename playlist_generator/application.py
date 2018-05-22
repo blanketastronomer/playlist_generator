4fe18657a75547e267d587a7c92df13e564a647a
Nicholas S <blanketastronomer@users.noreply.github.com>
2018-05-22 00:12:58 -0700

    Add application menu
    The load and save functions still need to be implemented.

--- playlist_generator/application.py
+++ playlist_generator/application.py
@@ -4,11 +4,25 @@ class Application():
     def __init__(self):
         self.main_window = Tk()
 
+    def load_playlist(self):
+        print("Loading playlist...")
+
+    def save_playlist(self):
+        print("Saving playlist...")
+
     def build_menu(self):
-        pass
+        menu = Menu(self.main_window)
+        self.main_window.config(menu=menu)
+
+        file_menu = Menu(menu)
+
+        menu.add_cascade(label="File", menu=file_menu)
+        file_menu.add_command(label="Load playlist", command=self.load_playlist)
+        file_menu.add_command(label="Save playlist", command=self.save_playlist)
 
     def build_ui(self):
-        pass
+        self.build_menu()
 
     def run(self):
+        self.build_ui()
         self.main_window.mainloop()
